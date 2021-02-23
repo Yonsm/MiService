9a2e8df1a16b888545f4881bae032894d6dd4c73
Yonsm <Yonsm@qq.com>
2021-02-23 12:49:55 +0800

    Refine 401

--- micom.py
+++ micom.py
@@ -19,18 +19,19 @@ class MiCom:
             method = 'GET' if data is None else 'POST'
             r = await self.auth.session.request(method, url, data=data, cookies=cookies, headers=headers)
             status = r.status
-            if status == 200 or status == 401:
-                if status == 200:
-                    resp = await r.json(content_type=None)
-                    code = resp['code']
-                    if code == 0:
-                        return resp
-                if relogin and status == 401 or 'auth' in resp.get('message', '').lower():
-                    _LOGGER.warn(f"Auth error on request {url}, relogin...")
-                    self.token = None  # Auth error, reset login
-                    return await self.request(sid, url, data, headers, False)
+            if status == 200:
+                resp = await r.json(content_type=None)
+                code = resp['code']
+                if code == 0:
+                    return resp
+                if 'auth' in resp.get('message', '').lower():
+                    status = 401
             else:
                 resp = await r.text()
+            if  status == 401 and relogin:
+                _LOGGER.warn(f"Auth error on request {url} {resp}, relogin...")
+                self.token = None  # Auth error, reset login
+                return await self.request(sid, url, data, headers, False)
         else:
             resp = "Login failed"
         error = f"Error {url}: {resp}"
